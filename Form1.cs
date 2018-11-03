e530ad7fc87eea832d256e3451cd5e3590464920
Ansersion <ansersion@gmail.com>
2018-11-03 10:05:16 +0800

    Add bplib.dll info for debug

--- Form1.cs
+++ Form1.cs
@@ -154,10 +154,15 @@ namespace BcTool
             // BPLibApi.BP_InitEmbededContext();
             string sn = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX3";
             string password = "3333333333333333333333333333333333333333333333333333333333333333";
+
             IntPtr snPtr = Marshal.AllocHGlobal(sn.Length + 1);
-            Marshal.Copy(System.Text.Encoding.ASCII.GetBytes(sn), 0, snPtr, sn.Length);
+            byte[] snBytes = System.Text.Encoding.ASCII.GetBytes(sn);
+            Marshal.Copy(snBytes, 0, snPtr, sn.Length);
+
             IntPtr passwordPtr = Marshal.AllocHGlobal(password.Length + 1);
-            Marshal.Copy(System.Text.Encoding.ASCII.GetBytes(password), 0, passwordPtr, password.Length);
+            byte[] passwordBytes = System.Text.Encoding.ASCII.GetBytes(password);
+            Marshal.Copy(passwordBytes, 0, passwordPtr, password.Length);
+
             IntPtr intPtrPack = BPLibApi.BP_PackConnect(ref bPContext, snPtr, passwordPtr);
             BPLibApi.PackBuf packBufSend = (BPLibApi.PackBuf)Marshal.PtrToStructure(intPtrPack, typeof(BPLibApi.PackBuf));
             byte[] sendBytes = new byte[packBufSend.MsgSize];
